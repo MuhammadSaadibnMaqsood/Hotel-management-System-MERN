23f42a44fd1f207c9fba172076ea5e7c9b14e0ae
Muhammad Saad <muhammadsaady0100@gmail.com>
2025-07-06 13:28:39 -0700

    sa

--- server/controller/clerkWebBook.js
+++ server/controller/clerkWebBook.js
@@ -1,46 +1,51 @@
+import User from '../models/user.js'
+import { Webhook } from "svix";
+
 const webBook = async (req, res) => {
-  try {
-    const wHook = new Webhook(process.env.CLERK_WEBBOOK_SECRET);
-    console.log(process.env.CLERK_WEBBOOK_SECRET);
+    try {
+        const wHook = new Webhook(process.env.CLERK_WEBBOOK_SECRET);
+        console.log(process.env.CLERK_WEBBOOK_SECRET);
 
-    const headers = {
-      'svix-id': req.headers['svix-id'],
-      'svix-timestamp': req.headers['svix-timestamp'],
-      'svix-signature': req.headers['svix-signature'],
-    };
+        const headers = {
+            'svix-id': req.headers['svix-id'],
+            'svix-timestamp': req.headers['svix-timestamp'],
+            'svix-signature': req.headers['svix-signature'],
+        };
 
-    console.log('Incoming Headers:', req.headers);
+        console.log('Incoming Headers:', req.headers);
 
-    // Clerk needs the raw body, NOT JSON.stringified
-    await wHook.verify(req.body, headers);
+        // Clerk needs the raw body, NOT JSON.stringified
+        await wHook.verify(req.body, headers);
 
-    const { data, type } = JSON.parse(req.body);
+        const { data, type } = JSON.parse(req.body);
 
-    const userData = {
-      _id: data.id,
-      email: data.email_addresses[0].email_address,
-      username: data.first_name + ' ' + data.last_name,
-      image: data.image_url,
-    };
+        const userData = {
+            _id: data.id,
+            email: data.email_addresses[0].email_address,
+            username: data.first_name + ' ' + data.last_name,
+            image: data.image_url,
+        };
 
-    switch (type) {
-      case 'user.created':
-        await User.create(userData);
-        break;
-      case 'user.updated':
-        await User.findByIdAndUpdate(data.id, userData);
-        break;
-      case 'user.deleted':
-        await User.findByIdAndDelete(data.id);
-        break;
-      default:
-        break;
+        switch (type) {
+            case 'user.created':
+                await User.create(userData);
+                break;
+            case 'user.updated':
+                await User.findByIdAndUpdate(data.id, userData);
+                break;
+            case 'user.deleted':
+                await User.findByIdAndDelete(data.id);
+                break;
+            default:
+                break;
+        }
+
+        res.json({ success: true, message: 'Webhook Received' });
+
+    } catch (error) {
+        console.log(error.message);
+        res.json({ success: false, message: error.message });
     }
-
-    res.json({ success: true, message: 'Webhook Received' });
-
-  } catch (error) {
-    console.log(error.message);
-    res.json({ success: false, message: error.message });
-  }
 };
+
+export default webBook;
